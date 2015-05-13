6ca6030c22ebca7240ccdeda53654f82597a3771
Venkat <Venkat@Nethra-PC>
2015-05-13 17:32:13 -0400

    update

---
+++ FirstProject.java/src/DataTypes.java
@@ -0,0 +1,31 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int myAge = Integer.parseInt("30");
+		byte urAge = 40;
+		
+		byte totalAge = (byte)(myAge+urAge);
+		
+		System.out.println(totalAge);
+		
+		
+		int height = myAge;
+		byte myheight = (byte)height;
+		
+		int ageofAnimal = (int)128;
+		System.out.println("ageofAnimal" + ageofAnimal);
+		
+		double heightofComputer = 5;
+		int heightofC = (int) heightofComputer;
+		System.out.println(heightofC);
+		
+		boolean isJayaramJAVAtutor = false;
+		
+		char readflag = 'r';
+		String name = "jayram" +readflag;
+	
+	}
+	
+
+}
