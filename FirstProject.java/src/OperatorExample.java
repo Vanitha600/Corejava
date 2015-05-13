19f19400121b0759c2a48378abb23b23705e298c
Venkat <Venkat@Nethra-PC>
2015-05-13 17:52:00 -0400

    update

---
+++ FirstProject.java/src/OperatorExample.java
@@ -0,0 +1,19 @@
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+			int numberOfDaysCount = 10;
+			
+			
+			System.out.println("numberOfDaysCount" +numberOfDaysCount);
+			numberOfDaysCount++;
+			
+			System.out.println("numberOfDaysCount" +numberOfDaysCount);
+			numberOfDaysCount = numberOfDaysCount+2;
+			
+			System.out.println("numberOfDaysCount" +numberOfDaysCount);
+			
+			
+	}
+
+}
