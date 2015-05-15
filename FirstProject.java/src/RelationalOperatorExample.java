ec4c5f48f8e48607af5fae05b98471aef38cef36
Venkat <Venkat@Nethra-PC>
2015-05-14 20:24:37 -0400

    Update

---
+++ FirstProject.java/src/RelationalOperatorExample.java
@@ -0,0 +1,18 @@
+
+public class RelationalOperatorExample {
+
+	public static void main(String[] args) {
+		int arg1 = Integer.parseInt(args[0]);
+		int arg2 = Integer.parseInt(args[1]);
+		
+		int age = Integer.parseInt(args[3]);
+		System.out.println(age);
+		
+		String height = 34+"";
+		
+		boolean isSame = (arg1 >= arg2);
+		System.out.println(isSame);
+		
+	}
+
+}
