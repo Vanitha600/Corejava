8d3f4ed3dcafd593945988072d6d4a5cbe0d18fa
Venkat <Venkat@Nethra-PC>
2015-05-28 18:29:28 -0400

    Update

---
+++ FirstProject.java/src/assignment/Class/BankEmployee.java
@@ -0,0 +1,17 @@
+package assignment.Class;
+
+public class BankEmployee {
+
+	public static void main(String[] args) {
+		String Employee1 = "Alen";
+		String Employee2 = "Anthem";
+		String Employee3 = "Tech";
+		
+		BankEmployee Employee1;
+		BankEmployee Employee2;
+		
+		System.out.println();
+		
+	}
+
+}
---
+++ FirstProject.java/src/assignment/Class/EmployeeAge.java
@@ -0,0 +1,12 @@
+package assignment.Class;
+
+class BankEmployee{
+
+public class EmployeeAge {
+
+	public static void main(String[] args) {
+		int EmployeeAge = 28;
+	}
+
+}
+}
